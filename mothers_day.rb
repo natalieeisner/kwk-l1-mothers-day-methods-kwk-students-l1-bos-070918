03186bf2bad241e586b2569a433ef965da2f38d6
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-10 18:00:02 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,4 +1,17 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
-def 
-
+part 1
+def mothers_day
+"Happy mothers day, #{name}! I hope you have a great day!"
+end
+puts mothers_day
+part 2
+def mothers_day(name)
+  "Happy mothers day, #{name}! I hope you have a great day!"
+end
+puts mothers_day("Ailsing")
+part 3
+def mothers_day(name="mom")
+  
+  
+  
+  
