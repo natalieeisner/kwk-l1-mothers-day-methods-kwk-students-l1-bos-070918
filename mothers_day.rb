58379f8ecc6c68e2e4a9ecb3a769dfe6bf236213
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-10 18:02:01 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,17 +1,20 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 part 1
 def mothers_day
-"Happy mothers day, #{name}! I hope you have a great day!"
+"Happy Mother's day, #{name}! I hope you have a great day!"
 end
 puts mothers_day
 part 2
 def mothers_day(name)
-  "Happy mothers day, #{name}! I hope you have a great day!"
+  "Happy Mother's day, #{name}! I hope you have a great day!"
 end
 puts mothers_day("Ailsing")
 part 3
 def mothers_day(name="mom")
+  "Happy Mother's day, #{name}!"
+end
   
-  
+  mothers_day("Aisling")
+  puts mothers_day
   
   
